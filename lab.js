305de08d0e96ea1fbff3792c9df8d9e948e5e522
Kaden Davis <kadentreyd@gmail.com>
2024-01-16 23:32:44 -0700

    Problem 8

--- lab.js
+++ lab.js
@@ -127,6 +127,11 @@ let passingScore = 7;
 
 //CODE HERE
 
+while (score < 7){
+  console.log("Your score is not high enough")
+  score += 1
+}
+
 ////////// INTERMEDIATE PROBLEMS //////////
 
 ////////// PROBLEM 9 //////////
